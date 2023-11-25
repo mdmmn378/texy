aa441597985eff14d455c3ed8f2ff5837e6d485c
mdmmn378 <mdmmn378@gmail.com>
2023-11-25 19:28:09 +0600

    Added tests

--- tests/py/test_par.py
+++ tests/py/test_par.py
@@ -18,7 +18,7 @@ def test_parallelize_with_native():
     """Test parallelize function."""
     data = ["a ", "b ", "c ", "d ", "e ", "f ", "g ", "h ?."]
     expected_data = ["a", "b", "c", "d", "e", "f", "g", "h"]
-    data *= 1_000
-    expected_data *= 1_000
+    data *= 10_000
+    expected_data *= 10_000
 
     assert parallelize(dummy, data, 2) == expected_data
